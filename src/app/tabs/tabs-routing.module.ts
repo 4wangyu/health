22fa9d6f80c2c229cc757f37fe8a669a8eb03a51
wangyu <4wangyu@gmail.com>
2019-11-17 14:41:57 +0800

    rename tab2 to data

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
           {
             path: "",
             loadChildren: () =>
-              import("../tab2/tab2.module").then(m => m.Tab2PageModule)
+              import("../data/data.module").then(m => m.DataPageModule)
           }
         ]
       },
