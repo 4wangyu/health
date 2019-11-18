50c7d5cb7c285aef468ac5e99e18cd15eb930b2c
wangyu <4wangyu@gmail.com>
2019-11-19 00:05:27 +0800

    add category

--- src/app/data/data.page.ts
+++ src/app/data/data.page.ts
@@ -15,6 +15,8 @@ export class DataPage implements OnInit {
   colors = Config.colors;
   cats = [];
   categories = [];
+  showForm = false;
+  input: string;
 
   ngOnInit(): void {
     const userRef = this.fbSvc.getUserRef();
@@ -35,5 +37,16 @@ export class DataPage implements OnInit {
   delete(cat: string) {
     this.cats = this.cats.filter(x => x != cat);
     this.fbSvc.updateCategories(this.cats);
+  }
+
+  updateInput(input: any) {
+    this.input = input.detail.value;
+  }
+
+  add() {
+    const string = this.input.toLowerCase();
+    this.cats.push(string.charAt(0).toUpperCase() + string.slice(1));
+    this.fbSvc.updateCategories(this.cats);
+    this.showForm = false;
   }
 }
